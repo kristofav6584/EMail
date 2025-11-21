278d6d180d5a31e591d9b424cd367530c4f6d96d
kristofav6584 <kovacsk3@kkszki.hu>
2025-11-21 11:20:55 +0100

    listázás

--- EMail/Program.cs
+++ EMail/Program.cs
@@ -2,8 +2,17 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            //adatszerkezet
+            List<String> e_mailok = new List<String>();
+            e_mailok.Add("alma1@kört.hu");
+            e_mailok.Add("alma2@dfdfdf.hu");
+            e_mailok.Add("alma3@ssdsds.hu");
+            e_mailok.Add("kovacsk3@kkszki.hu");
+            e_mailok.Add("alma.korte@alma.hu");
+
             //menüvezérlés
             while (true)
             {
@@ -16,8 +25,14 @@
                 string valasztas = Console.ReadLine();
                 switch (valasztas)
                 {
-                    case "0":return;
-                    case "1": Console.WriteLine("e - mailek listázása"); break;
+                    case "0": return;
+                    case "1": Console.WriteLine("e - mailek listázása");
+                        int sorszam = 0;
+                        foreach (String e_mail in e_mailok)
+                        {
+                            Console.WriteLine($"{sorszam++} {e_mail}");
+                        }
+                        break;
                     case "2": Console.WriteLine("új e - mailek"); break;
                     case "3": Console.WriteLine("e - mail törlés"); break;
                     case "4": Console.WriteLine("érvényes e - mailek listázása"); break;
