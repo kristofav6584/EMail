0915eb2da74ec2e043b3b066fd17c07edf4242ae
kristofav6584 <kovacsk3@kkszki.hu>
2025-11-21 11:06:48 +0100

    menü

--- EMail/Program.cs
+++ EMail/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            
+            //menüvezérlés
+            while (true)
+            {
+                Console.WriteLine("0. Kilépés");
+                Console.WriteLine("1. e-mailek listázása");
+                Console.WriteLine("2. új e-mail");
+                Console.WriteLine("3. e-mail törlése");
+                Console.WriteLine("4. érvényes e-mailok listázása");
+
+                string valasztas = Console.ReadLine();
+                switch (valasztas)
+                {
+                    case "0":return;
+                    case "1": Console.WriteLine("e - mailek listázása"); break;
+                    case "2": Console.WriteLine("új e - mailek"); break;
+                    case "3": Console.WriteLine("e - mail törlés"); break;
+                    case "4": Console.WriteLine("érvényes e - mailek listázása"); break;
+                    default: Console.Clear(); Console.WriteLine("rossz parancs"); break;
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
